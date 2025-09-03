c286bbd986f0388e847915841af2ddb0f72d825e
aortiz879 <aortiz879@email.uagm.edu>
2025-09-03 15:47:28 -0400

    Update BookList.java

--- BookList.java
+++ BookList.java
@@ -1,6 +1,6 @@
 /**
  * Louiz A. Inostroza Ruiz s01397648
- * Anais Ortiz Montanez
+ * Anais Ortiz Montanez    s01433872
  * Idaliedes Vergara
  * Project 2
  * 
@@ -100,3 +100,4 @@ public class BookList {
         return sb.toString();
     }
 }
+
